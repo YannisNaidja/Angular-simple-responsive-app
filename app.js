363aabc908a9adcb2bd4da1f6e8320773cd68641
YannisNaidja <yannis.naidja@gmail.com>
2021-01-09 21:06:30 +0100

    try to deploy

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
-const assert = require('assert');
+
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const treetagger = require('treetagger');
+
+
 const cors = require('cors');
 const http = require('http');
-const fs = require('fs');
-const request = require('request');
-var qs = require('querystring');
-const shell = require('shelljs');
-const { currentId } = require('async_hooks');
+
 const app = express();
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache({stdTTL: 100, checkperiod: 120});
 app.use(express.json());
-
 
 
 var corsOptions = {
@@ -40,11 +34,12 @@ app.get("/getRelationType/:word", cors(corsOptions), (req, res) => {
 
     //check si dans le cache
     
-
+  
     // avoir les types de relations associees à un mot
 
-    let word = req.params.word;
+    let word = escape(req.params.word);
 
+  
     console.log(word);
 
     var id_key= word+"_getRelationType";
@@ -72,7 +67,7 @@ app.get("/getRelationType/:word", cors(corsOptions), (req, res) => {
             //console.log(s);
             var relationtype = {
               id    :s.split(";")[1],
-              desc  :s.split(";")[3],
+              desc  :s.split(";")[3].replace(/'/g,''),
               tips   :s.split(";")[5],
               clicked : false     
           };
@@ -80,7 +75,7 @@ app.get("/getRelationType/:word", cors(corsOptions), (req, res) => {
             console.log(rez);
             RelationsTypesArray.push(relationtype);
         }
-
+        
         myCache.set(id_key,RelationsTypesArray,3600);
         res.end(JSON.stringify(RelationsTypesArray));
       });
@@ -90,52 +85,23 @@ app.get("/getRelationType/:word", cors(corsOptions), (req, res) => {
 
 
     
-   /* request("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-      word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
-      "&rel=", {
-        json: true
-      }, (err, res2, body) => {
-        if (err) {
-          return console.log(err);
-        }
-       // const regex = /((e;[0-9]+;.*)|(r;[0-9]+;.*))/m;
-        const regex = /(rt;[0-9]+;.*)/gm;
-        console.log(body.match(regex));
-        var relationstypes = body.match(regex);
-        var RelationsTypesArray = new Array();
-        for(var s of relationstypes){
-            //console.log(s);
-            var relationtype = {
-              id    :s.split(";")[1],
-              desc  :s.split(";")[3],
-              tips   :s.split(";")[5]          
-          };
-            var rez = s.split(";")[1]+" "+s.split(";")[3];
-            console.log(rez);
-            RelationsTypesArray.push(relationtype);
-        }
-
-       
-        res.end(JSON.stringify(RelationsTypesArray));
-    });*/
-    
-
 app.get("/getRelations/:word/", cors(corsOptions), (req, res) => {
 
   // avoir toutes les relations entrantes ou sortantes associées à ce mot 
 
-  let word= req.params.word;
+  let word= escape(req.params.word);
+  
 
   var id_key= word+"_getRelations";
 
-    if(myCache.has(id_key)){
+   if(myCache.has(id_key)){
         CachedData = myCache.get(id_key);
         res.end(JSON.stringify(CachedData));
     }
     else{
 
       http.get("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-      word + "&rel=36?gotermsubmit=Chercher&gotermrel=" + word +
+      word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
       "&rel=", (resp) => {
         resp.setEncoding('binary');
         let data = '';
@@ -165,35 +131,6 @@ app.get("/getRelations/:word/", cors(corsOptions), (req, res) => {
   });
 
       
-
-  /*request("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-    word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
-    "&rel=", {
-      json: true
-    }, (err, res2, body) => {
-      if (err) {
-        return console.log(err);
-      }
-     // const regex = /((e;[0-9]+;.*)|(r;[0-9]+;.*))/m;
-      const regex = /(r;[0-9]+;.*)/gm;
-      console.log(body.match(regex));
-      var nodes = body.match(regex);
-      var nodesArray = new Array();
-      for(var s of nodes){
-       
-          var node = {
-            idSource : s.split(";")[2],
-            idTarget : s.split(";")[3],
-            idRelationType : s.split(";")[4],
-            poidRelation : s.split(";")[5]
-            }
-            
-          nodesArray.push(node);
-        } 
-
-        myCache.set(id_key,nodesArray,3600);
-        res.end(JSON.stringify(nodesArray));
-        });*/
     }
 });
 
@@ -202,7 +139,8 @@ app.get("/getAssociatedNodes/:word/", cors(corsOptions), (req, res) => {
   // avoir les infos de tous les noeuds associés à un mot.
 
 
-  let word = req.params.word;
+  let word = escape(req.params.word);
+  
 
   var id_key= word+"_getAssociatedNodes";
 
@@ -215,7 +153,7 @@ app.get("/getAssociatedNodes/:word/", cors(corsOptions), (req, res) => {
     }else{
 
       http.get("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-      word + "&rel=36?gotermsubmit=Chercher&gotermrel=" + word +
+      word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
       "&rel=", (resp) => {
         resp.setEncoding('binary');
         let data = '';
@@ -231,7 +169,7 @@ app.get("/getAssociatedNodes/:word/", cors(corsOptions), (req, res) => {
            
               var node = {
                 id : s.split(";")[1],
-                name : s.split(";")[2]
+                name : s.split(";")[2].replace(/'/g,'')
                 }
     
               nodesArray.push(node);
@@ -242,34 +180,6 @@ app.get("/getAssociatedNodes/:word/", cors(corsOptions), (req, res) => {
     });
   });
 
-
-  
-  /*request("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-    word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
-    "&rel=", {
-      json: true
-    }, (err, res2, body) => {
-      if (err) {
-        return console.log(err);
-      }
-     // const regex = /((e;[0-9]+;.*)|(r;[0-9]+;.*))/m;
-      const regex = /(e;[0-9]+;.*)/gm;
-      console.log(body.match(regex));
-      var nodes = body.match(regex);
-      var nodesArray = new Array();
-      for(var s of nodes){
-       
-          var node = {
-            id : s.split(";")[1],
-            name : s.split(";")[2]
-            }
-
-          nodesArray.push(node);
-        } 
-    
-        myCache.set(id_key,nodesArray,3600); // dans le cache pour une heure
-        res.end(JSON.stringify(nodesArray));
-        });*/
     }
 });
 
@@ -278,8 +188,9 @@ app.get("/getAssociatedNodeById/:word/:id", cors(corsOptions), (req, res) => {
 
   // avoir les infos d'un noeud grâce à son id
 
-  let word= req.params.word;
+  let word= escape(req.params.word);
   let id = req.params.id;
+  
 
   http.get("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
       word + "&rel=36?gotermsubmit=Chercher&gotermrel=" + word +
@@ -308,41 +219,15 @@ app.get("/getAssociatedNodeById/:word/:id", cors(corsOptions), (req, res) => {
           res.end(JSON.stringify("Pas de noeud associe a ce mot pour cet id."));
         });
       });
-
-
-  /*request("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-    word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
-    "&rel=", {
-      json: true
-    }, (err, res2, body) => {
-      if (err) {
-        return console.log(err);
-      }
-     // const regex = /((e;[0-9]+;.*)|(r;[0-9]+;.*))/m;
-      const regex = /(e;[0-9]+;.*)/gm;
-      console.log(body.match(regex));
-      var nodes = body.match(regex);
-      
-      for(var s of nodes){
-        let currentid = s.split(";")[1];
-        if(currentid===id){
-          var node = {
-            id : currentid,
-            name : s.split(";")[2]
-            }
-          res.end(JSON.stringify(node));
-        } 
-      }
-      res.end(JSON.stringify("Pas de noeud associe a ce mot pour cet id."));
-  });*/
 });
 
 app.get("/getAssociatedRelationsById/:word/:id", cors(corsOptions), (req, res) => {
 
   // avoir les relations d'un mot par l'id de le relation
 
-  let word= req.params.word;
+  let word= escape(req.params.word);
   let id = req.params.id;
+ 
 
   var id_key=word+"_"+id+"_getAssociatedRelationsById";
 
@@ -383,49 +268,18 @@ app.get("/getAssociatedRelationsById/:word/:id", cors(corsOptions), (req, res) =
   });
 
           
-        
-
-  
-
-
-      
-
-  /*request("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-    word + "&rel=?gotermsubmit=Chercher&gotermrel=" + word +
-    "&rel=", {
-      json: true
-    }, (err, res2, body) => {
-      if (err) {
-        return console.log(err);
-      }
-     // const regex = /((e;[0-9]+;.*)|(r;[0-9]+;.*))/m;
-      const regex = /(r;[0-9]+;.*)/gm;
-      console.log(body.match(regex));
-      var nodes = body.match(regex);
-      var nodesArray = new Array();
-      for(var s of nodes){
-        let currentid = s.split(";")[4];
-        if(currentid===id){
-          var node = {
-            idSource : s.split(";")[2],
-            idTarget : s.split(";")[3],
-            idRelationType : s.split(";")[4],
-            poidRelation : s.split(";")[5]
-            }
-            nodesArray.push(node);
-        } 
-      }
-      myCache.set(id_key,nodesArray,3600);
-      res.end(JSON.stringify(nodesArray));
-        });*/
       
 app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
 
-        let word= req.params.word;
-        quotedword = "'"+word+"'";
+        let word = req.params.word;
+        let encodedword= escape(word);
+        
+        quotedword = "'"+encodedword+"'";
+
+        console.log("dans word id reçu: " +encodedword);
 
         http.get("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
-      word + "&rel=36?gotermsubmit=Chercher&gotermrel=" + word +
+      encodedword + "&rel=36?gotermsubmit=Chercher&gotermrel=" + encodedword +
       "&rel=", (resp) => {
         resp.setEncoding('binary');
         let data = '';
@@ -433,16 +287,19 @@ app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
           data += chunk;
         });
         resp.on('end', () => {
+          //console.log("data de wordid : "+data);
           const regex = /(e;[0-9]+;.*)/gm;
           console.log(data.match(regex));
           var nodes = data.match(regex);
-            for(var s of nodes){
-            console.log("comparaison de "+s.split(";")[2]+" avec "+ word);
-            if(s.split(";")[2]===   quotedword){
+          if(nodes!=null){
+            var s = nodes[0];
+            console.log("comparaison de "+s.split(";")[2]+" avec "+ quotedword);
+            if(s.split(";")[2]===quotedword){
+              console.log("trouve l'id");
               res.end(JSON.stringify(s.split(";")[1]));
             } 
-          }
-          res.end(JSON.stringify("id du mot non trouvee"));
+        }
+
         });
     });
 
@@ -450,7 +307,7 @@ app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
 
     app.get("/getWordRel1/:word", cors(corsOptions), (req, res) => {
 
-        let word= req.params.word;
+        let word= escape(req.params.word);
         quotedword = "'"+word+"'";
 
         var id_key=word+"_getWordRel1";
@@ -471,7 +328,7 @@ app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
         resp.on('end', () => {
           const regex = /(e;[0-9]+;.*)/gm;
           const regexrel = /(r;[0-9]+;.*)/gm;
-          console.log(data.match(regex));
+          //console.log(data.match(regex));
           var nodes = data.match(regex);
 
           var raffArray = new Array();
@@ -479,7 +336,8 @@ app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
                 if(s.split(";")[2].includes(word+">")){
                     console.log(s.split(";")[2]);
                     var node = {
-                        raff : s.split(";")[2].replace(/'/g,'')
+                        raffid : s.split(";")[2].replace(/'/g,''),
+                        raffdesc : s.split(";")[5].replace(/'/g,'')
                     }
                     raffArray.push(node)
                 }
@@ -494,7 +352,8 @@ app.get("/getWordId/:word", cors(corsOptions), (req, res) => {
 
 app.get("/getDef/:word", cors(corsOptions), (req, res) => {
 
-    let word= req.params.word;
+    let encodedword = escape(req.params.word);
+    let word = req.params.word;
     quotedword = "'"+word+"'";
 
     var id_key=word+"_getDef";
@@ -503,8 +362,7 @@ app.get("/getDef/:word", cors(corsOptions), (req, res) => {
         CachedData = myCache.get(id_key);
         res.end(JSON.stringify(CachedData));
         }else{
-    var encodedword = encodeURI(word);
-    console.log(encodedword);
+    
     http.get("http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=" +
   encodedword + "&rel=?gotermsubmit=Chercher&gotermrel=" + encodedword +
   "&rel=", (resp) => {
@@ -515,29 +373,37 @@ app.get("/getDef/:word", cors(corsOptions), (req, res) => {
     });
     resp.on('end', () => {
     
+    const regexname = /(e;[0-9]+;.*)/gm;
     const regex = /<def>(.|\n)*?<\/def>/;
     //const regex = /(?:<def>)([\s\S]*)(?:<\/def>)/;
     var content = data.match(regex);
-
-    var node = {
-        value : content
+    //console.log(content);
+    var newcontent = content.toString().replace(/<def>/,'');
+    newcontent = newcontent.toString().replace(/<br\s\/>/gm,'');
+    newcontent = newcontent.toString().replace("</def>,",'');
+    console.log(newcontent)
+    var namecontent = data.match(regexname);
+    var name = namecontent[0];
+    //presence d'un texte pour la def du raffinement
+    if(name.split(";")[5]!==undefined){
+      var node = {
+        id : name.split(";")[5].replace(/'/g,''),
+        value : newcontent,
+        show : false
+      }
     }
+    else{
+      var node = {
+        value : newcontent,
+        show : false
+      }
+
+    }
+    
 
     res.end(JSON.stringify(node));
 
-    /*  var defArray = new Array();
-        for(var s of nodes){
-            if(s.split(";")[2].includes(word+">")){
-                console.log(s.split(";")[2]);
-                var node = {
-                    def : s.split(";")[2]
-                }
-                defArray.push(node)
-            }
-           
-      }*/
-      //myCache.set(id_key,defArray,3600);
-      //res.end(JSON.stringify(defArray));
+   //todo cache
         });
     });
 }
